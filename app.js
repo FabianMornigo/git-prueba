43d39b7ac4dd259017e4be73e9e52335401944af
Fabián <Fabianmorinigo92@gmail.com>
2018-07-22 00:53:40 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+		/*var h2 = document.getElementById("titulo");
+		
+		console.log(h2.innerHTML);
+
+		h2.innerHTML = "Nuevo";
+
+		h2.style.color="green";*/
+
+		document.write('Ingrese sus datos');
+		//alert(document.title);
+
+		var notaFinal;
+		notaFinal	 = 10;
+
+		notaFinal = notaFinal += 5 ;
+
+		//alert(notaFinal);
+
+
+		function msg(m){
+
+			alert(m);
+		}
+
+		function sumatoria(n){
+			return n + n;
+		}
